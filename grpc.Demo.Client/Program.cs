2604b9f34900e1ec441d4aa6842e31fccceb2c54
jeserra <joser_isi@hotmail.com>
2024-05-24 09:22:31 -0600

    Initial commit

---
+++ grpc.Demo.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+
+using grpc.Demo;
+using grpc.Demo.Protos;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Comunicandome con el grpc");
+
+
+PaymentRequest request = new PaymentRequest()
+{
+    Id = 2,
+    Sender = "Miguel de Lys",
+    Receiver = "Ubisoft",
+    Amount = 200
+};
+
+var channel = GrpcChannel.ForAddress("http://localhost:5247");
+var client = new Payment.PaymentClient(channel);
+var result = await client.SendMoneyAsync(request);
+
+Console.WriteLine(result.Message);
+
+
+
---
+++ grpc.Demo/Services/PaymentService.cs
@@ -0,0 +1,16 @@
+﻿using grpc.Demo.Protos;
+using Grpc.Core;
+
+namespace grpc.Demo.Services;
+
+public class PaymentService: Payment.PaymentBase
+{
+    public override Task<PaymentResponse> SendMoney(PaymentRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new PaymentResponse()
+        {
+            Message = $"Pago recibido de {request.Sender} a {request.Receiver} exitosamente",
+            Status = 10
+        }); 
+    }
+}
